861bab9d51a7d89e4fa50da69b74138f59f00ca6
Colin Rundel <rundel@gmail.com>
2026-03-17 09:49:46 -0400

    Make update

--- static/slides/_setup.R
+++ static/slides/_setup.R
@@ -9,16 +9,15 @@ knitr::opts_chunk$set(
 local({
   hook_err_old <- knitr::knit_hooks$get("error")  # save the old hook
   knitr::knit_hooks$set(error = function(x, options) {
-    # now do whatever you want to do with x, and pass
-    # the new x to the old hook
     x = sub("## \n## Detailed traceback:\n.*$", "", x)
     x = sub("Error in py_call_impl\\(.*?\\)\\: ", "", x)
     hook_err_old(x, options)
   })
-  
+
   hook_warn_old <- knitr::knit_hooks$get("warning")  # save the old hook
   knitr::knit_hooks$set(warning = function(x, options) {
     x = sub("<string>:1: ", "", x)
+    x = stringr::str_wrap(x)
     hook_warn_old(x, options)
   })
 })
